a7b6547295769458b408c297118d7a6f62b23aee
Bogdane Braoun <112286@ecole-it.com>
2026-02-25 15:07:59 +0100

    API data foraging func added in script.js

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-const API_KEY = "7cab0c291c8c4c00b52c3959d264b03a"
+const API_KEY = `7cab0c291c8c4c00b52c3959d264b03a`;
+const URL = `https://newsapi.org/v2/everything?domains=techcrunch.com&language=en&apiKey=${API_KEY}`;
+const Elm = document.querySelector("div")
+async function getData(){
+    let response = await fetch(URL);
+    let data = await response.json();
+    console.log(data);
+    data["articles"].forEach(element => {
+        console.log(element);
+        Elm.innerHTML = `
+    <div>
+        <div>
+            <h2>
+                ${element["title"]}
+            </h2>
+        </div>
+        <div>
+            <p>
+                ${element["description"]}
+            </p>
+        </div>
+        <div>
+            <p>
+                Source: ${element["source"]["name"]}
+            </p>
+        </div>
+        <div>
+            <a href="${element["url"]}"> Lire Plus</a>
+        </div>
+    </div>
+    `
+    });
+    
+};
 
-const url = `https://newsapi.org/v2/everything&apiKey=${API_KEY}`
+getData()
